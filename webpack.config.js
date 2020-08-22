b82e8c5d55269192d44a0bbeaf0f86c1873847ad
Grigory Kaniev <netsafe.g@gmail.com>
2020-08-22 16:45:59 +0500

    add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
+const isDev = process.env.NODE_ENV === 'development';
+
 /** @type {import('webpack').Configuration} */
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -10,6 +12,10 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: isDev ? 'source-map' : '',
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
     },
     plugins: [
         new CleanWebpackPlugin(),
